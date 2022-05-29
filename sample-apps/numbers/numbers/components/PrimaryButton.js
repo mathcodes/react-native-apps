fb680bdd7d2f9742d90339cb9c9fcf3d000a64e9
Jon Christie <jonpchristie@gmail.com>
2022-05-29 15:03:17 -0400

    styling button

--- sample-apps/numbers/numbers/components/PrimaryButton.js
+++ sample-apps/numbers/numbers/components/PrimaryButton.js
@@ -1,13 +1,34 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 
 
 export default function PrimaryButton({children}) {
+
+  function pressHandler() {
+    console.log('pressed')
+  }
   return (
-    <View>
-      <Text>
-        {children}
-      </Text>
-    </View>
+    <Pressable onPress={pressHandler} android_ripple={{color: '#000'}}>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>
+          {children}
+        </Text>
+      </View>
+    </Pressable>
   );
 }
+
+let styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: '#345507',
+    borderRadius: 28,
+    paddingVertical: 8, 
+    paddingHorizontal: 16,
+    margin: 4,
+    elevation: 2
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  }
+})
--- sample-apps/numbers/numbers/screens/StartGameScreen.js
+++ sample-apps/numbers/numbers/screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from '../components/PrimaryButton';
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput 
+        style={styles.numberInput} 
+        maxLength={2} 
+        keyboardType='number-pad'
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -14,8 +20,27 @@ export default function StartGameScreen() {
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
+    // flex: 1,
     marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius :7,
     padding: 16,
+    backgroundColor: '#d35400',
+    elevation: 4, //android 
+    shadowColor: '#000', //ios ...
+    shadowOffset: {width: 3, height: 6},
+    shadowRadius: 7,
+    shadowOpacity: 1 // ... ios
+  },
+  numberInput: {
+    height: 50,
+    width: 60,
+    fontSize: 32,
+    borderBottomColor: '#ddbf43',
+    borderBottomWidth: 2,
+    color: '#ddbf43',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
   }
 })
