c695750365ed804c09c136e3b5a4f306f58996ce
Jon Christie <jonpchristie@gmail.com>
2022-06-03 16:30:12 -0400

    useEffect more ui and game components

--- sample-apps/numbers/numbers/components/ui/Title.js
+++ sample-apps/numbers/numbers/components/ui/Title.js
@@ -1,5 +1,5 @@
 import { Text, StyleSheet } from 'react-native';
-import Colors from '../constants/colors';
+import Colors from '../../constants/colors';
 function Title({children}) {
   return <Text style={styles.title}>{children}</Text>
 }
--- sample-apps/numbers/numbers/constants/colors.js
+++ sample-apps/numbers/numbers/constants/colors.js
@@ -1,9 +1,10 @@
 // setup a regular js object which assigns colors to keys
 const Colors = {
-  primary500: '#B26700',
+  primary500: '#E06D06',
   primary600: '#640233',
+  primary700: '#B26700',
   primary800: '#161032',
-  accent500: '#FFC53A'
+  accent500: '#FAFF81'
 }
 
 export default Colors;
