57a5cc57931cd1d01342cdee096a0e7966665550
Jon Christie <jonpchristie@gmail.com>
2022-06-04 08:24:19 -0400

    last before nav apps

---
+++ sample-apps/numbers/numbers/constants/colors.android.js
@@ -0,0 +1,10 @@
+// setup a regular js object which assigns colors to keys
+const Colors = {
+  primary500: '#9eca54',
+  primary600: '#9bcd5d',
+  primary700: '#709a53',
+  primary800: '#d6d8de',
+  accent500: '#4e2b92'
+}
+
+export default Colors;
