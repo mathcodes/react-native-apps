f8a27d913fb1f6d8bb230e7c7bb178ce8694909b
Jon Christie <jonpchristie@gmail.com>
2022-06-08 10:42:13 -0400

    added 3 more rows of buttons and styled them to display as rows

--- sample-apps/rn-calculator/src/components/Keyboard.tsx
+++ sample-apps/rn-calculator/src/components/Keyboard.tsx
@@ -7,32 +7,50 @@ export default function MyKeyboard() {
 
   return (
     <View style={Styles.viewBottom}>
-    <View
-      style={{
-        alignSelf: "center",
-        justifyContent: "flex-end",
-        height: 100,
-        width: "100%",
-      }}
-    >
-      
+      <View style={Styles.viewChild}>
         <Text style={{ color: "gray", width: "80%", fontSize: 20, fontWeight: '900' }}>😎</Text>
-      
+      </View>
+      <View style={Styles.row}>
+        <Button title="b1" />
+        <Button title="b2" />
+        <Button title="b3" />
+        <Button title="b4" />
+      </View>
+      <View style={Styles.row}>
+        <Button title="b1" />
+        <Button title="b2" />
+        <Button title="b3" />
+        <Button title="b4" />
+      </View>
+      <View style={Styles.row}>
+        <Button title="b1" />
+        <Button title="b2" />
+        <Button title="b3" />
+        <Button title="b4" />
+      </View>
+      <View style={Styles.row}>
+        <Button title="b1" />
+        <Button title="b2" />
+        <Button title="b3" />
+        <Button title="b4" />
+      </View>
     </View>
-    <View>
-      <Button title="b1"/>
-      <Button title="b2"/>
-      <Button title="b3"/>
-      <Button title="b4"/>
-    </View>
-   
-  </View>
   );
 }
 
 export const Styles = StyleSheet.create({
-    viewBottom: {
-        position: 'absolute',
-        bottom: 50,
-    }
+  viewBottom: {
+    position: 'absolute',
+    bottom: 50,
+  },
+  viewChild: {
+    alignSelf: "center",
+    justifyContent: "flex-end",
+    height: 100,
+    width: "100%"
+  },
+  row: {
+    maxWidth: '100%',
+    flexDirection: "row",
+  }
 })
