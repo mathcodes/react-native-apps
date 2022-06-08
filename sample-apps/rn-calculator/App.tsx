afd6bddf2d521202c49e787f3bc7373c8f382abc
Jon Christie <jonpchristie@gmail.com>
2022-06-08 08:55:06 -0400

    dark mode working

--- sample-apps/rn-calculator/App.tsx
+++ sample-apps/rn-calculator/App.tsx
@@ -1,16 +1,28 @@
-import { Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Switch, Text } from 'react-native';
+import { globalColors } from './src/styles/colors';
+import { ThemeContext } from './src/context/ThemeContext';
+
+
 
 export default function App() {
-  
+  const [theme, setTheme] = useState('light');
   return (
-    <Text>App.jsx running</Text>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+        <Switch
+          value={theme === 'dark'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        />
+      </SafeAreaView>
+    </ThemeContext.Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'white',
+    backgroundColor: globalColors.light,
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
---
+++ sample-apps/rn-calculator/src/styles/colors.ts
@@ -0,0 +1,9 @@
+export const globalColors ={
+  light: '#cecece',
+  dark: '#373737',
+  blue: '#4faeba',
+  gray: '#777777',
+  black: '#000000',
+  white: '#FFFFFF', 
+  result: '#c737a5',
+}
