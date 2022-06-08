c3e8294c600956cb54a26dbb4669c48f65a5e672
Jon Christie <jonpchristie@gmail.com>
2022-06-08 11:01:40 -0400

    ui setup, time for logic to render user input and solution

--- sample-apps/rn-calculator/src/components/Keyboard.tsx
+++ sample-apps/rn-calculator/src/components/Keyboard.tsx
@@ -8,14 +8,14 @@ export default function MyKeyboard() {
   const [firstNumber, setFirstNumber] = React.useState("");
   const [secondNumber, setSecondNumber] = React.useState("");
   const [operation, setOperation] = React.useState("");
-  const [result, setResult] = React.useState<Number | null >(null);
+  const [solution, setSolution] = React.useState<Number | null>(null);
 
 
   const clear = () => {
     setFirstNumber("");
     setSecondNumber("");
     setOperation("");
-    setResult(null);
+    setSolution(null);
   };
 
   // Handlers;
@@ -25,6 +25,10 @@ export default function MyKeyboard() {
 
   const opPressHandler = (buttonValue: string) => {
 
+  };
+
+  const getSolution = () => {
+    // switch case
   };
 
   return (
@@ -46,23 +50,30 @@ export default function MyKeyboard() {
         <Button title="÷" isBlue onPress={() => opPressHandler("/")} />
       </View>
       <View style={Styles.row}>
-        <Button title="b1" />
-        <Button title="b2" />
-        <Button title="b3" />
-        <Button title="b4" />
+        <Button title="7" onPress={() => numPressHandler("7")} />
+        <Button title="8" onPress={() => numPressHandler("8")} />
+        <Button title="9" onPress={() => numPressHandler("9")} />
+        <Button title="×" isBlue onPress={() => opPressHandler("*")} />
       </View>
       <View style={Styles.row}>
-        <Button title="b1" />
-        <Button title="b2" />
-        <Button title="b3" />
-        <Button title="b4" />
+        <Button title="4" onPress={() => numPressHandler("4")} />
+        <Button title="5" onPress={() => numPressHandler("5")} />
+        <Button title="6" onPress={() => numPressHandler("6")} />
+        <Button title="-" isBlue onPress={() => opPressHandler("-")} />
       </View>
       <View style={Styles.row}>
-        <Button title="b1" />
-        <Button title="b2" />
-        <Button title="b3" />
-        <Button title="b4" />
+        <Button title="1" onPress={() => numPressHandler("1")} />
+        <Button title="2" onPress={() => numPressHandler("2")} />
+        <Button title="3" onPress={() => numPressHandler("3")} />
+        <Button title="+" isBlue onPress={() => opPressHandler("+")} />
       </View>
+      <View style={Styles.row}>
+        <Button title="." onPress={() => numPressHandler(".")} />
+        <Button title="0" onPress={() => numPressHandler("0")} />
+        <Button title="⌫" onPress={() => setFirstNumber(firstNumber.slice(0, -1))} />
+        <Button title="=" isBlue onPress={() => getSolution()} />
+      </View>
+
     </View>
   );
 }
