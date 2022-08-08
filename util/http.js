ccc7d55e3c8bbffc711c00dada1788f4b246d7d3
Jon Christie <jonchristie@Jons-MBP.attlocal.net>
2022-08-08 02:18:47 -0400

    file org

--- util/http.js
+++ util/http.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export function storeExpense(expenseData) {
-  axios.post('https://react-native-6a4d4-default-rtdb.firebaseio.com/expenses.json',  // Fire base just wants this .json at the end here to understand that we're targeting a specific node in the database. So to say.
+  axios.post('https://react-native-6a4d4-default-rtdb.firebaseio.com/expenses.json', 
   expenseData 
   );
-  
+}
